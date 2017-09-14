fea8a1126d5f85a0327828a3ade48492b3674688
roy769 <roy@mortgallery.com>
2017-09-14 13:28:42 -0700

    redo

---
+++ bamazon.js
@@ -0,0 +1,35 @@
+var mysql = require('mysql');
+
+var inquirer = require("inquirer");
+
+var connection = mysql.createConnection
+({
+  host: 'localhost',
+  port: 3306,
+
+  user: 'root',
+
+  password: 'Insecure',
+  database: 'bamazon_DB',
+});
+
+
+  connection.connect(function(err)
+   {
+    if (err) throw err;
+      begin();
+  });
+
+ connection.connect(function(err)
+ {
+    if (err) throw err;
+    begin();
+ });
+
+function begin()
+  {
+    inquirer.prompt
+    ({
+
+    })
+  }
